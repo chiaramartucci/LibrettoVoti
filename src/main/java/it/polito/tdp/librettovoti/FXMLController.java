7e10b3cb55c737e53675d0d6bea0d52097aa0313
Asus <Asus@DESKTOP-B15PUDQ>
2021-03-17 12:57:05 +0100

    trasformazione MVC (primi passi)

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,68 @@
 package it.polito.tdp.librettovoti;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
+	private Libretto model;
+	
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TextField txtEsame;
+
+    @FXML
+    private TextField txtVoto;
+
+    @FXML
+    private TextField txtData;
+
+    @FXML
+    private TextArea result;
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	// leggi e controlla i dati 
+    	String nomeEsame = txtEsame.getText();
+    	String votoEsame = txtVoto.getText();
+    	int votoInt = Integer.parseInt(votoEsame);
+    	
+    	String dataEsame = txtData.getText();
+    	LocalDate data = LocalDate.parse(dataEsame);
+    	// TODO: aggiungere tutti i controlli
+    	
+    	// esegui l'azione
+    	Voto voto = new Voto (nomeEsame, votoInt, data);
+    	model.add(voto);
+    	
+    	// aggiorna i risultati (nella View)
+    	result.setText(model.toString());
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model=model;
+    }
+
+    @FXML
+    void initialize() {
+        assert txtEsame != null : "fx:id=\"txtEsame\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert result != null : "fx:id=\"result\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
+
